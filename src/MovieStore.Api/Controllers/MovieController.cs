c92825634e8cd9f21532b49d42cbe8570cd9fec3
Rania <raniayaqoubi1995@gmail.com>
2024-04-22 23:50:13 +0200

    new project structure

---
+++ src/MovieStore.Api/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieStore.ServiceLibrary.Domains;
+using MovieStore.ServiceLibrary.Entities;
+
+namespace MovieStore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult AddNewMovie([FromQuery]MovieEntity movieEntity)
+        {
+            var businessLogic = new Movie();
+            businessLogic.SaveMovie(movieEntity);
+            return Ok();
+        }
+
+    }
+
+
+
+   
+}
