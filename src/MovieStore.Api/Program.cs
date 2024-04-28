c6c287b54df97592088fdc2570b8546429f13538
Rania <raniayaqoubi1995@gmail.com>
2024-04-28 16:59:48 +0200

    saving last changes + moving Dockerfile into upper folder

--- src/MovieStore.Api/Program.cs
+++ src/MovieStore.Api/Program.cs
@@ -14,7 +14,7 @@ internal class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
